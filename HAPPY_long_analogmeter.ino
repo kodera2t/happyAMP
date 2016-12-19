674fdba82041e6971533d11c1cf1a78f1fe1f163
kodera2t <kodera2t@yahoo.co.jp>
2016-12-19 11:13:25 +0900

    waveform display function added

--- HAPPY_long_analogmeter.ino
+++ HAPPY_long_analogmeter.ino
@@ -1,6 +1,7 @@
 /// Happy amplifier firmware
 /// input:A0 and A1
 /// Dec. 2016 kodera2t
+/// waveform display mode added
 
 #include <SPI.h>
 #include <Wire.h>
@@ -12,12 +13,13 @@ Adafruit_SSD1306 display(OLED_RESET);
 
 int mode,modeflag,oldxval,oldyval;
 unsigned long ct;
-
-
+  int xpos,ypos;   
+  int cn1,averagenum;
+  
 void setup()   {
+
   attachInterrupt(0,mode_setting,CHANGE);
-  pinMode(2, INPUT);
-  int xpos,ypos;                
+  pinMode(2, INPUT);             
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   // init done
@@ -46,6 +48,7 @@ void setup()   {
 ////////////////////////////////
   display.display();
   mode=0;
+  averagenum=100;
 }
 
 
@@ -55,31 +58,41 @@ void loop() {
   float voltage;
   left=0;
   right=0;
-  for(int i=0;i<100;i++){
+  for(int i=0;i<averagenum;i++){
+  if(mode !=3){
   readvalue1=abs((analogRead(A0)-512)*10);
   readvalue2=abs((analogRead(A1)-512)*10);
+  }else{
+  readvalue1=((analogRead(A0)-512)*10)/10;
+  readvalue2=((analogRead(A1)-512)*10)/10;    
+  }
   voltage=3.3*readvalue1/1024.0;
   left=left+readvalue1;
   right=right+readvalue2;
   }
-  readvalue1=left/100;
-  readvalue2=right/100;
+  readvalue1=left/averagenum;
+  readvalue2=right/averagenum;
   switch(mode){
   case 0:
+  averagenum=130;
   drawaxis();
   drawneedle(readvalue1,readvalue2);
     eraseneedle(readvalue1,readvalue2);
- // showvalue(readvalue1,readvalue2);
-  //delay(200);
   break;
   case 1:
+  averagenum=100;
   drawaxis();
   drawneedle(readvalue1,readvalue2);
     eraseneedle(readvalue1,readvalue2);
   showvalue(readvalue1,readvalue2);  
   break;
   case 2:
+  averagenum=300;
   showbar(readvalue1,readvalue2);
+  break;  
+  case 3:
+  averagenum=1;
+  plotgrh(readvalue1, readvalue2);
   break;
   }
 }
@@ -122,6 +135,7 @@ void drawaxis(){
 
 void drawneedle(int i,int j){
   int xpos2,ypos2;
+
   xpos2=16-10*cos(i/1024.0*3.1415);
   ypos2=16-10*sin(i/1024.0*3.1415);
   display.drawLine(xpos2,ypos2,16,16,WHITE);
@@ -133,6 +147,7 @@ void drawneedle(int i,int j){
 
 void eraseneedle(int i,int j){
   int xpos3,ypos3;
+
   xpos3=16-10*cos(i/1024.0*3.1415);
   ypos3=16-10*sin(i/1024.0*3.1415);
   display.drawLine(xpos3,ypos3,16,16,BLACK);
@@ -157,6 +172,8 @@ void showvalue(int i,int j){
 }
 
 void showbar(int i,int j){
+  i=abs(i);
+  j=abs(j);
   if(modeflag==1){
     display.clearDisplay();
     modeflag=0;
@@ -181,14 +198,63 @@ void mode_setting(){
   int sw,k,pt;
   ct=millis();
   sw=digitalRead(2);
-if(sw==LOW && (ct-pt)>50){
+if(sw==LOW && (ct-pt)>100){
 modeflag=1;
 mode=mode+1;
 }
-  if(mode>2){
+  if(mode>3){
     mode=0;
   }
 }
+
+void plotgrh(int i, int j){
+
+  if(modeflag==1){
+    display.clearDisplay();
+    display.display();
+    cn1=0;
+    modeflag=0;
+  }
+//  left[cn1]=i;
+//  right[cn1]=j;
+  cn1=cn1+1;
+//       display.clearDisplay();
+//      display.setCursor(0,0);
+//    display.print(left[10]);
+//    display.setCursor(0,8);
+//    display.print(right[10]);
+//    display.display();
+
+//  if(cn1==44){
+//    for(int k=0;k<45;k++){
+////     display.clearDisplay();
+//     display.drawPixel(k,8+left[k]/30,WHITE);  
+//     display.drawPixel(k+48,8+right[k]/30,WHITE);
+//     display.display();   
+//    }
+//    
+////    display.setCursor(0,0);
+////    display.print(left[22]);
+////    display.setCursor(0,16);
+////    display.print(right[22]);
+////    display.display();
+//  cn1=0;
+//  display.fillRect(0,0,96,16,BLACK);
+//  }
+
+  display.drawPixel(cn1,8+i/30,WHITE);
+  display.drawPixel(cn1+48,8+j/30,WHITE);
+  //display.display();
+
+  if(cn1==44){
+    display.display();
+    cn1=0;
+    display.fillRect(0,0,96,16,BLACK);
+  }
+  
+}
+
+
 
 
 
